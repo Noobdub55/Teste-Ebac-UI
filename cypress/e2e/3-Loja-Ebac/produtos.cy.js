e64d5480daa799462a5cd560733ef1998621b480
matheus lima <matheus19win@gmail.com>
2025-08-26 17:58:42 -0300

    atualização teste de produto

---
+++ cypress/e2e/3-Loja-Ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('deve selecionar produto da lista', () => {
+        cy.get(' .product-block')
+          // .first()
+           // .last()
+            // .eq(4)
+             .contains('Ariel Roll Sleeve Sweatshirt')
+              .click()
+
+                cy.get('#tab-title-description > a').should('exist')
+    });
+    
+});
