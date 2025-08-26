3dd3201affd6f5d5fec0a9a80c531be1a5e946e0
matheus lima <matheus19win@gmail.com>
2025-08-26 18:19:15 -0300

    atualização cadastro com variaveis

---
+++ cypress/e2e/3-Loja-Ebac/cadastro.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress"/>
+import { faker } from '@faker-js/faker'
+
+describe('Funcionalidade: Casdastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('Deve fazer cadastro com sucesso', () => {
+
+        var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+        
+
+        cy.get('[name="email"]').type(email)
+        cy.get('.register > :nth-child(2) > [name="password"]').type('chama123')
+        cy.get('[name="register"]').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('[name="account_first_name"]').type(nome)
+        cy.get('[name="account_last_name"]').type(sobrenome)
+        cy.get('[name="save_account_details"]').click()
+        cy.get('.woocommerce-message').should('exist')
+        
+    });
+
+});
